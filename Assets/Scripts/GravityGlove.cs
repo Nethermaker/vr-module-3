57a0c459ac571dda9e686987ef66148278ba5d7a
Nethermaker <tsenne2@huskers.unl.edu>
2021-02-17 17:20:32 -0600

    Add object launch calculations, add SteamVR Input

--- Assets/Scripts/GravityGlove.cs
+++ Assets/Scripts/GravityGlove.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
+using static UnityEngine.Random;
+using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class GravityGlove : MonoBehaviour
@@ -17,11 +19,20 @@ public class GravityGlove : MonoBehaviour
     [Header("You can change these")]
     [SerializeField] private float distance;
     [SerializeField] private float size;
+    [SerializeField] private Vector3 rotationOffset;
+    [SerializeField] private float travelTime;
     private Vector3 center;
     private Vector3 direction;
     private Vector3 scale;
+    private Quaternion rotation;
 
-    private GameObject currentThrowable;
+    private bool isGrabbing;
+
+    private Hand hand;
+    private SteamVR_Action_Boolean grabPinch = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "grabPinch");
+    private SteamVR_Action_Boolean grabGrip = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "grabGrip");
+
+    private GameObject targettedThrowable;
     private GameObject previousThrowable;
     private GameObject activeThrowable;
     
@@ -29,23 +40,51 @@ public class GravityGlove : MonoBehaviour
     {
         //Use this to ensure that the Gizmos are being drawn when in Play Mode.
         mStarted = true;
+
+        hand = GetComponent<Hand>();
+    }
+
+    private void Update()
+    {
+        isGrabbing = hand.grabGripAction.state || hand.grabPinchAction.state;
+        // isGrabbing = Input.GetKey(KeyCode.Mouse0);
     }
 
     private void FixedUpdate()
     {
-        currentThrowable = SelectThrowable();
-        Debug.Log(currentThrowable.name);
+        targettedThrowable = SelectThrowable();
+
+        if (null != targettedThrowable)
+        {
+            targettedThrowable.GetComponent<MeshRenderer>().material.color = ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+
+        if (isGrabbing && null != targettedThrowable && targettedThrowable != activeThrowable)
+        {
+            targettedThrowable.GetComponent<Rigidbody>().velocity = GetLaunchVelocity(targettedThrowable.transform.position, transform.position, travelTime);
+            activeThrowable = targettedThrowable;
+            StartCoroutine(DeactiveThrowable(travelTime + 0.5f));
+        }
+        isGrabbing = false;
+    }
+
+    private IEnumerator DeactiveThrowable(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        activeThrowable = null;
     }
 
     private GameObject SelectThrowable()
     {
         // TODO: This `direction` will probably have to change when attached to hands
-        direction = transform.forward; 
+        direction = Quaternion.Euler(rotationOffset) * transform.forward; 
         center = transform.position + direction * distance / 2;
         scale = new Vector3(size, size, distance);
+        //rotation = transform.rotation * Quaternion.Euler(rotationOffset);
+        rotation = transform.rotation;
         
         // Get all colliders within the box we've created and loop over each
-        Collider[] hitColliders = Physics.OverlapBox(center, scale / 2, transform.rotation, mLayerMask, QueryTriggerInteraction.Ignore);
+        Collider[] hitColliders = Physics.OverlapBox(center, scale / 2, rotation, mLayerMask, QueryTriggerInteraction.Ignore);
         GameObject best = null;
         float bestScore = Single.PositiveInfinity;
         foreach (Collider collider in hitColliders)
@@ -74,11 +113,20 @@ public class GravityGlove : MonoBehaviour
     
     private Vector3 GetLaunchVelocity(Vector3 start, Vector3 target, float time)
     {
-        // This is simpler than I though it would be.
-        // We'll know distance between the two points, and we'll know time to get there
-        // Horizontal velocity can be determined from that and vertical velocity doesn't matter
-        //  as long as it doesn't hit the ground before it reaches the target
-        return Vector3.zero;
+        // Calculate horizontal launch velocity
+        Vector3 horizontalOffset = new Vector3(target.x, 0f, target.z) - new Vector3(start.x, 0f, start.z);
+        float speed = horizontalOffset.magnitude / time; // Required speed in units/s
+        Vector3 horizontalVelocity = horizontalOffset.normalized * speed;
+
+        // Calculate Vertical launch velocity
+        float verticalOffset = target.y - start.y;
+        Debug.Log(verticalOffset);
+        float gravity = Physics.gravity.y;
+        float verticalSpeed = Math.Abs(gravity) * (time / 2) + verticalOffset;
+
+        Vector3 verticalVelocity = new Vector3(0f, verticalSpeed, 0f);
+
+        return horizontalVelocity + verticalVelocity;
     }
 
     //Draw the Box Overlap as a gizmo to show where it currently is testing. Click the Gizmos button to see this
@@ -88,6 +136,6 @@ public class GravityGlove : MonoBehaviour
         //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
         if (mStarted)
             //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireMesh(drawMesh, center, transform.rotation, scale);
+            Gizmos.DrawWireMesh(drawMesh, center, rotation, scale);
     }
 }
