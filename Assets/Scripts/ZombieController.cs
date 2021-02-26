17844a990ea7f06eb143940e1e0163bec464d808
Joel Matzen <BackTrack4545@gmail.com>
2021-02-26 11:31:04 -0600

    collision detection

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ZombieFollow : MonoBehaviour
+public class ZombieController : MonoBehaviour
 {
     
     public Transform transformToFollow;
-   
+    public Animator animator;
+    
+
     NavMeshAgent agent;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
@@ -19,5 +24,10 @@ public class ZombieFollow : MonoBehaviour
     {
         //Follow the player
         agent.destination = transformToFollow.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
     }
 }
